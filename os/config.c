qtmlabs/xfs

commit 4a163a076e90808fa47ade1504fd0f2893bdc306

Return an error to the log instead of segfaulting
Return an error to the log instead of segfaulting
if catalogue line is omitted from config file.

http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=148650

os/config.c
@@ -296,6 +296,10 @@ SetConfigValues(void)
     int         err,
                 num;
 
+    if (font_catalogue = NULL) {
+	FatalError("font catalogue is missing/empty\n");
+    }
+
     err = SetFontCatalogue(font_catalogue, &num);
     if (err != FSSuccess) {
 	FatalError("element #%d (starting at 0) of font path is bad or has a bad font:\n\"%s\"\n",

commit 8144d75d4e71d8ae0493043f49fe9925f8099244

Fix nasty typo in SetConfigValues
Nasty typo introduced in 4a163a076e90808fa47ade1504fd0f2893bdc306,
causes xfs to crash on startup.
http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=482551

os/config.c
@@ -296,7 +296,7 @@ SetConfigValues(void)
     int         err,
                 num;
 
-    if (font_catalogue = NULL) {
+    if (font_catalogue == NULL) {
 	FatalError("font catalogue is missing/empty\n");
     }
 