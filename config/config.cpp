293b8b9163083e7d8adfde752a29e25436b927a1
Gaetan Nadon <memsize@videotron.ca>
2010-08-05 08:02:40 -0400

    Using the C preprocessor to generate "config" is no longer needed.
    Reuse and adapt the man makefile using sed in a seperate directory
    Replace XCOMM IMakefile comment with #
    
    Reviewed-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Signed-off-by: Gaetan Nadon <memsize@videotron.ca>

--- config/config.cpp
+++ config/config.cpp
@@ -1,10 +1,10 @@
-XCOMM X font server configuration file
-XCOMM See xfs(__appmansuffix__) man page for more information.
+# X font server configuration file
+# See xfs(__appmansuffix__) man page for more information.
 
 clone-self = on
 use-syslog = off
 catalogue = DEFAULTFONTPATH
 error-file = FSERRORS
-XCOMM in decipoints
+# in decipoints
 default-point-size = 120
 default-resolutions = 75,75,100,100
