9ad39a265b093793a0d9f009018f99f3fde596bf
Gaetan Nadon <memsize@videotron.ca>
2010-08-05 13:31:57 -0400

    fontcache: remove unreachable code around FONTCACHE define
    Unlike the xset app, xfs has never defined or included a header
    that defines FONTCACHE, even when compiled with libXfont 1.3.3
    which uses fontcacheproto and defines FONTCACHE in its config.h.
    
    This will allow further simplification when generating config.cpp.
    
    Signed-off-by: Gaetan Nadon <memsize@videotron.ca>

--- config.cpp
+++ config.cpp
@@ -8,10 +8,3 @@ error-file = FSERRORS
 XCOMM in decipoints
 default-point-size = 120
 default-resolutions = 75,75,100,100
-
-#ifdef FONTCACHE
-XCOMM font cache control, specified in KB
-cache-hi-mark = 2048
-cache-low-mark = 1433
-cache-balance = 70
-#endif /* FONTCACHE */
