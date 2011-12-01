03b4a543a812b14a565a92ac691cdc0f550e95d1
Alan Coopersmith <alan.coopersmith@oracle.com>
2011-11-30 18:21:30 -0800

    Call malloc, not xalloc
    One call in xfstrans.c used xalloc - everything else already had malloc.
    Fix this so it won't break when Xtrans stops defining xalloc to malloc
    for us.
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- os/xfstrans.c
+++ os/xfstrans.c
@@ -51,7 +51,7 @@ TRANS(GetInetdListenInfo) (int fd)
     else
 #endif
 	portnum = ntohs(((struct sockaddr_in *)(inetdCI->addr))->sin_port);
-    inetdCI->port = xalloc(6); /* Base 10 integer <= 65535 + trailing NUL */
+    inetdCI->port = malloc(6); /* Base 10 integer <= 65535 + trailing NUL */
     snprintf(inetdCI->port, 6, "%d", portnum);
 
     /* Do the socket setup that xtrans normally takes care of in
